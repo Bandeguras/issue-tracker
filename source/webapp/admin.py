24926552cc25d2f6961c1597537be320fb98ae6e
Said Saliev <repsaid@gmail.com>
2022-11-17 20:37:52 +0600

    create models and migrations

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from webapp.models import Task, Status, Type
 # Register your models here.
+
+class TYPE_STATUS_ADMIN(admin.ModelAdmin):
+    list_display = ['name']
+class TASK_ADMIN(admin.ModelAdmin):
+    list_display = ['summary']
+
+admin.site.register(Task, TASK_ADMIN)
+admin.site.register(Status, TYPE_STATUS_ADMIN)
+admin.site.register(Type, TYPE_STATUS_ADMIN)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+TYPES_CHOICES = [('Task', 'задача'), ('Bug', 'ошибка'), ('Enhancement', 'улучшение')]
+CATEGORY_CHOICES = [('New', 'новый'), ('In Progress', 'в процессе'), ('Done', 'выполнено')]
+
 
 # Create your models here.
+class Task(models.Model):
+    summary = models.CharField(max_length=30, verbose_name="Заголовок")
+    description = models.TextField(max_length=3000, verbose_name="Описание", null=True, blank=True)
+    status = models.ForeignKey('webapp.Status', on_delete=models.PROTECT, related_name='statuses')
+    type = models.ForeignKey('webapp.Type', on_delete=models.PROTECT, related_name='types')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+
+class Type(models.Model):
+    name = models.CharField(max_length=30, choices=TYPES_CHOICES, verbose_name="Типы")
+class Status(models.Model):
+    name = models.CharField(max_length=30, choices=CATEGORY_CHOICES, verbose_name="Статус")
