5caf0f097d969a9b4b55c1645ad70883e607f656
Said Saliev <repsaid@gmail.com>
2022-11-17 23:00:54 +0600

    add detailed view page

--- source/main/urls.py
+++ source/main/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from webapp.views import IndexViews
+from webapp.views import IndexViews, TaskView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexViews.as_view(), name='index'),
+    path('task/<int:pk>', TaskView.as_view(), name='task'),
+
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from webapp.models import Task
 from django.views import View
+from django.views.generic import TemplateView
 # Create your views here.
 
 
@@ -11,3 +12,11 @@ class IndexViews(View):
             'tasks': tasks,
         }
         return render(request, 'index.html', context)
+
+class TaskView(TemplateView):
+    template_name = 'task_view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['task'] = get_object_or_404(Task, pk=kwargs.get('pk'))
+        return context
