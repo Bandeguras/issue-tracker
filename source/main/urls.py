c2c4b70da5ce40c116e095206a57bfdce4239094
Said Saliev <repsaid@gmail.com>
2022-11-17 21:55:18 +0600

    add main page and base

--- source/main/urls.py
+++ source/main/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import IndexViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexViews.as_view(), name='index'),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from webapp.models import Task
+from django.views import View
 # Create your views here.
+
+
+class IndexViews(View):
+    def get(self, request, *args, **kwargs):
+        tasks = Task.objects.all()
+        context = {
+            'tasks': tasks,
+        }
+        return render(request, 'index.html', context)
