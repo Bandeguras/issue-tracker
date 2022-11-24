b590c186d4300657e0dccb30b0863fb43f1d021e
Said Saliev <repsaid@gmail.com>
2022-11-24 21:58:20 +0600

    form model form

--- source/webapp/form.py
+++ source/webapp/form.py
@@ -1,11 +1,10 @@
 from django import forms
-from webapp.models import Type, Status
+from webapp.models import Task
 from django.forms import widgets
 
 
-class TaskForm(forms.Form):
-    summary = forms.CharField(max_length=30, label="Summary", required=True)
-    description = forms.CharField(max_length=3000, label="Description", required=False,
-                                  widget=widgets.Textarea(attrs={"cols": 20, "rows": 3}))
-    status = forms.ModelChoiceField(queryset=Status.objects.all(), label="Status", required=True)
-    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), label="Type", required=True)
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['summary', 'description', 'status', 'type']
+        widgets = {'type': widgets.CheckboxSelectMultiple}
