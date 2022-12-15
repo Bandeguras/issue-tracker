b0e63c0498dc6da705af1b58c07e55f92da28639
Said Saliev <repsaid@gmail.com>
2022-12-15 17:27:46 +0600

    add login

---
+++ source/accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from accounts.views import login_view
+
+app_name = 'accounts'
+urlpatterns = [
+    path('accounts/login', login_view, name='login')
+]
+
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+def login_view(request):
+    context = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('webapp:project_index')
+        else:
+            context['has_error'] = True
+    return render(request, 'login.html', context=context)
+
