f62b4c86cb49ac41bc74d386c13b131b18104f1a
Said Saliev <repsaid@gmail.com>
2022-12-18 14:15:35 +0600

    add validation

--- source/accounts/form.py
+++ source/accounts/form.py
@@ -13,6 +13,12 @@ class MyUserCreationForm(UserCreationForm):
 
     def clean(self):
         cleaned_data = super().clean()
-        if Q(cleaned_data['first_name']) | Q(cleaned_data['last_name']) is "":
+        if (cleaned_data['first_name'] or cleaned_data['last_name']) == '':
             raise ValidationError('At least one of the fields (firstname, lastname) must be filled in.')
         return cleaned_data
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if email == '':
+            raise ValidationError('Email is required')
+        return email
