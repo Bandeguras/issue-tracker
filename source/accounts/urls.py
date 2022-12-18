c0d51565f1a25b6ef2dfbfa99ca8cfb19aca11cc
Said Saliev <repsaid@gmail.com>
2022-12-18 12:37:16 +0600

    fix mistake

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,10 +1,22 @@
 from django.urls import path
+
 from accounts.views import login_view, logout_view
+from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = 'accounts'
-urlpatterns = [
-    path('accounts/login', login_view, name='login'),
-    path('accounts/logout', logout_view, name='logout')
 
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
+
+# from django.urls import path
+# from accounts.views import login_view, logout_view
+#
+# app_name = 'accounts'
+# urlpatterns = [
+#     path('accounts/login', login_view, name='login'),
+#     path('accounts/logout', logout_view, name='logout')
+#
+# ]
 
