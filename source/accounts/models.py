411d41a91e4f8b04f880556a178491549c6e7b0f
Said Saliev <repsaid@gmail.com>
2022-12-26 20:29:57 +0600

    create profile model add Pillow library

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), related_name='profile', on_delete=models.CASCADE, verbose_name='Пользователь')
+    avatar = models.ImageField(blank=True, null=True, upload_to='user_avatar', verbose_name="Аватар")
+    git = models.URLField(blank=True, null=True, max_length=256, verbose_name="ГитХаб")
+    description = models.TextField(blank=True, null=True, max_length=3000, verbose_name="О себе")
+
+    def __str__(self):
+        return f"{self.user.get_full_name()} 's Profile "
+
+    class Meta:
+        db_table = 'profile'
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
--- source/main/settings.py
+++ source/main/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_ROOT = Path.joinpath(BASE_DIR, 'uploads')
+MEDIA_URL = '/media/'
