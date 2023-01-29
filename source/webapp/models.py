9c541da31b8c71f912c7f4da3bf9fb74f06d1768
Said Saliev <repsaid@gmail.com>
2023-01-29 21:17:13 +0600

    add in Project and Task class new line 'Likes'

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -12,6 +12,7 @@ class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата начала")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата окончания")
     project = models.ForeignKey('webapp.Project', on_delete=models.CASCADE, related_name='projects')
+    likes = models.ManyToManyField(get_user_model(), related_name='tasks_likes', verbose_name='Лайки')
 
     def get_absolute_url(self):
         return reverse('webapp:task', kwargs={'pk': self.pk})
@@ -37,6 +38,7 @@ class Project(models.Model):
     expiration_at = models.DateField(verbose_name="Время изменения", null=True, blank=True)
     summary = models.CharField(max_length=30, verbose_name="Заголовок")
     description = models.TextField(max_length=3000, verbose_name="Описание")
+    likes = models.ManyToManyField(get_user_model(), related_name='projects_likes', verbose_name='Лайки')
 
     def get_absolute_url(self):
         return reverse('webapp:project', kwargs={'pk': self.pk})
